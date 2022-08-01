9067e16d3694f4b3f2fb901666b07158f9e755e3
BonnieAxelsen <104758739+BonnieAxelsen@users.noreply.github.com>
2022-08-01 09:24:20 +0200

    Added to responsiveness, attempted js for menu

--- script/script.js
+++ script/script.js
@@ -1,5 +1,36 @@
-$(".menu-icon").click(function () {
-  $(this).toggleClass("active");
-  $(".navigation-menu").toggleClass("active");
-  $(".menu-icon i").toggleClass("fa-times");
-});
+function mainMenuButtonClicked() {
+  let openMenuMain = document.querySelector("#menu");
+  const bodyDiv = document.body;
+  if (window.getComputedStyle(openMenuMain).getPropertyValue("opacity") != 0) {
+    openMenuMain.classList.remove("menu-unfolded");
+    bodyDiv.style.overflow = "visible";
+  } else {
+    openMenuMain.classList.add("menu-unfolded");
+    bodyDiv.style.overflow = "hidden";
+  }
+}
+
+function mobileMenuButtonClicked() {
+  console.log("Hello this works");
+  let openMenuMain = document.querySelector("#menu-mobile");
+  const bodyDiv = document.body;
+  if (window.getComputedStyle(openMenuMain).getPropertyValue("opacity") != 0) {
+    openMenuMain.classList.remove("menu-unfolded-mobile");
+    bodyDiv.style.overflow = "visible";
+  } else {
+    openMenuMain.classList.add("menu-unfolded-mobile");
+    bodyDiv.style.overflow = "hidden";
+  }
+}
+
+let mainMenuButton = document.querySelector("#menu-icon-main");
+mainMenuButton.addEventListener("click", mainMenuButtonClicked);
+
+let openMenuButton = document.querySelector("#menu-icon");
+openMenuButton.addEventListener("click", mainMenuButtonClicked);
+
+let mobileMenuButton = document.querySelector("#menu-icon-main-mobile");
+mobileMenuButton.addEventListener("click", mobileMenuButtonClicked);
+
+let openMobileMenuButton = document.querySelector("#menu-icon-mobile");
+openMobileMenuButton.addEventListener("click", mobileMenuButtonClicked);
